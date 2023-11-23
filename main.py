80409340de527b92df21592848bea90f61750746
outrunme <148824509+outrunme@users.noreply.github.com>
2023-11-23 22:23:32 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import numpy as np
+from os.path import exists
+
+G = 6.67430 * 10 ** (-11)
 
 number = int(input("Select how many bodies you want: "))
 time_step = float(
@@ -9,94 +12,145 @@ lengthofsimul = float(
 )
 print("The following simulation assumes point-like masses")
 print("Distances in AU and mass in solar-mass")
-abs_pos = [[0.0 for j in range(2)] for i in range(number)]
-rel_pos = [[[0.0 for k in range(2)] for j in range(number)] for i in range(number)]
-velocity = [[0.0 for j in range(2)] for i in range(number)]
-acceleration = [[0.0 for j in range(2)] for i in range(number)]
-mass = [0] * number
+
+# Initialize Measurable quantities
+abs_pos = [[0.0, 0.0, 0.0] for i in range(number)]
+rel_pos = [[[0.0, 0.0, 0.0] for j in range(number)] for i in range(number)]
+velocity = [[0.0, 0.0, 0.0] for i in range(number)]
+mass = [[0.0] for i in range(number)]
+accln = [[0.0, 0.0, 0.0] for i in range(number)]
+
+# Take Values from file
+if exists("nbodyparameters.csv"):
+    f = open("nbodyparameters", "r")
+    pass
 
 
-def AcceptPosition(position):
+# Accept initial parameters
+def AcceptParameters(position, velocity, mass):
     for i in range(number):
-        for j in range(2):
-            position[i][j] = float(
-                input(
-                    "Please input Co-ordinate {} of particle {}: ".format(j + 1, i + 1)
-                )
+        # Position
+        position[i][0] = float(
+            input("Please input Co-ordinate {} of particle {}: ".format(1, i + 1))
+        )
+        position[i][1] = float(
+            input("Please input Co-ordinate {} of particle {}: ".format(2, i + 1))
+        )
+        position[i][2] = float(
+            input("Please input Co-ordinate {} of particle {}: ".format(3, i + 1))
+        )
+        # Velocity
+        velocity[i][0] = float(
+            input(
+                "Please input Velocity Component {} of particle {}: ".format(1, i + 1)
             )
-    return position
-
-
-def AcceptVelocity(velocity):
-    for i in range(number):
-        for j in range(2):
-            velocity[i][j] = float(
-                input(
-                    "Please input Velocity Component {} of particle {}: ".format(
-                        j + 1, i + 1
-                    )
-                )
+        )
+        velocity[i][1] = float(
+            input(
+                "Please input Velocity Component {} of particle {}: ".format(2, i + 1)
             )
-    return velocity
+        )
+        velocity[i][2] = float(
+            input(
+                "Please input Velocity Component {} of particle {}: ".format(3, i + 1)
+            )
+        )
+        # Mass
+        mass[i] = float(input("Please input Mass of particle {}: ".format(i + 1)))
+    return position, velocity, mass
 
 
-def AcceptMass(mass):
+# Get acceleration
+def findaccln(position, ds, accln):
     for i in range(number):
-        print("PLease input Mass of particle", (i + 1))
-        mass[i] = float(input())
-    return mass
-
-
-def definerelativeposition(relativepostion, position):
-    for i in range(number):
+        accln[i][0] = 0
+        accln[i][1] = 0
+        accln[i][2] = 0
         for j in range(number):
-            for k in range(2):
-                relativepostion[i][j][k] = position[i][k] - position[j][k]
-    return relativepostion
+            if i > j:
+                # Find Relative Position
+                ds[i][j][0] = position[i][0] - position[j][0]
+                ds[i][j][1] = position[i][1] - position[j][1]
+                ds[i][j][2] = position[i][2] - position[j][2]
+                dist = (
+                    ((ds[i][j][0]) ** 2) + ((ds[i][j][1]) ** 2) + ((ds[i][j][2]) ** 2)
+                ) ** (3 / 2)
+                accx = -(0.00596 * mass[j] * ds[i][j][0]) / dist
+                accy = -(0.00596 * mass[j] * ds[i][j][1]) / dist
+                accz = -(0.00596 * mass[j] * ds[i][j][2]) / dist
+                accln[i][0] = accln[i][0] + (accx)
+                accln[i][1] = accln[i][1] + (accy)
+                accln[i][2] = accln[i][2] + (accz)
+                accln[j][0] = accln[j][0] - (mass[i] / mass[j]) * (accx)
+                accln[j][1] = accln[j][1] - (mass[i] / mass[j]) * (accy)
+                accln[j][2] = accln[j][2] - (mass[i] / mass[j]) * (accz)
+    return ds, accln
 
 
-def findacceleration(relativeposition, acceleration):
+abs_pos, velocity, mass = AcceptParameters(abs_pos, velocity, mass)
+rel_pos, accln = findaccln(abs_pos, rel_pos, accln)
+
+
+# Update
+def updateproperties(position, velocity, accln, timestep, ds):
+    ds, accln = findaccln(abs_pos, rel_pos, accln)
+    # print(accln)
     for i in range(number):
+        velocity[i][0] = velocity[i][0] + accln[i][0] * timestep
+        velocity[i][1] = velocity[i][1] + accln[i][1] * timestep
+        velocity[i][2] = velocity[i][2] + accln[i][2] * timestep
+        position[i][0] = position[i][0] + velocity[i][0] * timestep / (
+            1.496 * (10 ** (11))
+        )
+        position[i][1] = position[i][1] + velocity[i][1] * timestep / (
+            1.496 * (10 ** (11))
+        )
+        position[i][2] = position[i][2] + velocity[i][2] * timestep / (
+            1.496 * (10 ** (11))
+        )
+    return position, velocity, accln, ds
+
+
+Kinetic_Energy = [[0] for i in range(number)]
+Potential_Energy = [[0.0 for j in range(number)] for k in range(number)]
+
+
+# Calculate Energies
+def energy(position, ds, velocity, mass, G):
+    for i in range(number):
+        # Kinetic energy here
+        Kinetic_Energy[i] = (
+            (
+                0.5
+                * mass[i]
+                * (velocity[i][0] ** 2 + velocity[i][1] ** 2 + velocity[i][2] ** 2)
+            )
+            * 1.989
+            * (10**30)
+        )
         for j in range(number):
-            for k in range(2):
-                if not i == j:
-                    acceleration[i][k] = acceleration[i][k] + ((0.005929 * mass[j])) * (
-                        -relativeposition[i][j][k]
-                    ) / (
-                        (relativeposition[i][j][0]) ** 2
-                        + ((relativeposition[i][j][1]) ** 2)
-                    ) ** (
-                        3 / 2
-                    )
-    return acceleration
+            if i > j:
+                # Potential energy here
+
+                ds[i][j][0] = position[i][0] - position[j][0]
+                ds[i][j][1] = position[i][1] - position[j][1]
+                ds[i][j][2] = position[i][2] - position[j][2]
+                dist = (
+                    ((ds[i][j][0]) ** 2) + ((ds[i][j][1]) ** 2) + ((ds[i][j][2]) ** 2)
+                ) ** (1 / 2)
+                Potential_Energy[i][j] = -(
+                    G * mass[i] * mass[j] * ((1.989 * (10**30)) ** 2) / dist
+                )
+    total_energy = sum(Kinetic_Energy) + sum(sum(Potential_Energy, []))
+    return total_energy
 
 
-abs_pos = AcceptPosition(abs_pos)
-velocity = AcceptVelocity(velocity)
-mass = AcceptMass(mass)
-rel_pos = definerelativeposition(rel_pos, abs_pos)
-acceleration = findacceleration(rel_pos, acceleration)
-
-
-# update
-def updateproperties(position, velocity, acceleration, timestep, relativeposition):
-    for i in range(number):
-        for j in range(2):
-            position[i][j] = position[i][j] + velocity[i][j] * timestep
-            velocity[i][j] = velocity[i][j] + acceleration[i][j] * timestep
-            acceleration = findacceleration(relativeposition, acceleration)
-    return position, velocity, acceleration
-
-
-# abs_pos, velocity = updateproperties(
-#     abs_pos, velocity, acceleration, time_step, rel_pos
-# )
+# Display properties
 for k in range(round(lengthofsimul / time_step)):
-    abs_pos, velocity, acceleration = updateproperties(
-        abs_pos, velocity, acceleration, time_step, rel_pos
+    abs_pos, velocity, accln, rel_pos = updateproperties(
+        abs_pos, velocity, accln, time_step, rel_pos
     )
-    print(abs_pos, velocity, acceleration)
-
-print(abs_pos)
-print(velocity)
-print(acceleration)
+    print(abs_pos)
+    # # Use To verify the code
+    # total_energy = energy(abs_pos, rel_pos, velocity, mass, G)
+    # print(total_energy)
